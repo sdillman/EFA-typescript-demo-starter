7c81b820965a40f8bbea632a7563d7990d6e1c47
Shannon D <sdillman@users.noreply.github.com>
2021-08-18 23:59:52 -0400

    through 2.11

--- app/03-classes.ts
+++ app/03-classes.ts
@@ -26,3 +26,86 @@ phil.sayHelloToFullname('Sheriff', 'Holler');
 let kenn: Person = new Person();
 kenn.firstName = "Kenn";
 kenn.sayHello();
+
+// 2.10 CONSTRUCTORS
+
+class Game {
+    constructor(name: string, maker: string) {
+        this.gameName = name;
+        this.gameMaker = maker;
+    }
+    gameName: string;
+    gameMaker: string;
+}
+let battleship: Game = new Game('Battleship', 'Hasbro');
+// We create a new class called Game.
+
+// We create a constructor for the class that takes in a parameter that is of type string.
+
+// We set up a property called gameName.
+
+// We set the value of gameName equal to the value that is passed into the constructor's argument.
+
+// We create an instance of the Game class called battleship.
+
+// We pass in values to satisfy the requirements of the constructor. If we do not pass values in for the constructor's arguments, we get an error. The following code throws a compile-time error
+
+
+// With the no-constructor class Person we instantiate the class with no constructor.
+
+// Then, we have to set each of the properties, one at a time. 
+phil.firstName = "Phil";
+phil.lastName = "Donahue";
+// With a constructor this could have been done in one line of code, as in battleship above
+
+// 2.11 INHERITANCE
+
+class Vehicle {
+    type: string;
+    make: string;
+    model: string;
+    manufactureYear: number;
+    runs: boolean;
+    topSpeed: number;
+}
+// Here we have what's called a parent class. This holds all the properties that are going to be universal to all vehicles. With inheritance, we can make subclasses of Vehicle that hold these properties along with the properties that are unique to the subclass. Here is the syntax for a subclass with two examples:
+
+// EXAMPLE ONE
+class Automobile extends Vehicle {
+    isSelfDriving: boolean;
+}
+let myCar: Automobile = new Automobile();
+myCar.isSelfDriving = false;
+myCar.topSpeed = 200;
+myCar.make = "Volvo";
+
+// EXAMPLE TWO
+
+class Motorcycle extends Vehicle {
+    easyToDoWheelie: boolean;
+}
+let myMotorcycle: Motorcycle = new Motorcycle();
+myMotorcycle.easyToDoWheelie = true;
+myMotorcycle.topSpeed = 180;
+
+class Animal {
+    laysEggs: boolean;
+    isLandDwelling: boolean;
+    furMarkings: string;
+}
+
+class Tiger extends Animal {
+    isObligateCarnivore: boolean;
+}
+class Bear extends Animal {
+    hibernationStartMonth: number;
+}
+
+let smokeyTheBear: Bear = new Bear();
+smokeyTheBear.furMarkings = 'solid';
+smokeyTheBear.hibernationStartMonth = 11;
+
+let tonyTheTiger: Tiger = new Tiger();
+tonyTheTiger.furMarkings = 'striped';
+tonyTheTiger.isObligateCarnivore = true;
+
--- js/03-classes.js
+++ js/03-classes.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person() {
     }
@@ -23,4 +38,79 @@ phil.sayHelloToFullname('Sheriff', 'Holler');
 var kenn = new Person();
 kenn.firstName = "Kenn";
 kenn.sayHello();
+// 2.10 CONSTRUCTORS
+var Game = /** @class */ (function () {
+    function Game(name, maker) {
+        this.gameName = name;
+        this.gameMaker = maker;
+    }
+    return Game;
+}());
+var battleship = new Game('Battleship', 'Hasbro');
+// We create a new class called Game.
+// We create a constructor for the class that takes in a parameter that is of type string.
+// We set up a property called gameName.
+// We set the value of gameName equal to the value that is passed into the constructor's argument.
+// We create an instance of the Game class called battleship.
+// We pass in values to satisfy the requirements of the constructor. If we do not pass values in for the constructor's arguments, we get an error. The following code throws a compile-time error
+// With the no-constructor class Person we instantiate the class with no constructor.
+// Then, we have to set each of the properties, one at a time. 
+phil.firstName = "Phil";
+phil.lastName = "Donahue";
+// With a constructor this could have been done in one line of code, as in battleship above
+// 2.11 INHERITANCE
+var Vehicle = /** @class */ (function () {
+    function Vehicle() {
+    }
+    return Vehicle;
+}());
+// Here we have what's called a parent class. This holds all the properties that are going to be universal to all vehicles. With inheritance, we can make subclasses of Vehicle that hold these properties along with the properties that are unique to the subclass. Here is the syntax for a subclass with two examples:
+// EXAMPLE ONE
+var Automobile = /** @class */ (function (_super) {
+    __extends(Automobile, _super);
+    function Automobile() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Automobile;
+}(Vehicle));
+var myCar = new Automobile();
+myCar.isSelfDriving = false;
+myCar.topSpeed = 200;
+myCar.make = "Volvo";
+// EXAMPLE TWO
+var Motorcycle = /** @class */ (function (_super) {
+    __extends(Motorcycle, _super);
+    function Motorcycle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Motorcycle;
+}(Vehicle));
+var myMotorcycle = new Motorcycle();
+myMotorcycle.easyToDoWheelie = true;
+myMotorcycle.topSpeed = 180;
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    return Animal;
+}());
+var Tiger = /** @class */ (function (_super) {
+    __extends(Tiger, _super);
+    function Tiger() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Tiger;
+}(Animal));
+var Bear = /** @class */ (function (_super) {
+    __extends(Bear, _super);
+    function Bear() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Bear;
+}(Animal));
+var smokeyTheBear = new Bear();
+smokeyTheBear.furMarkings = 'solid';
+smokeyTheBear.hibernationStartMonth = 11;
+var tonyTheTiger = new Tiger();
+tonyTheTiger.furMarkings = 'striped';
+tonyTheTiger.isObligateCarnivore = true;
 //# sourceMappingURL=03-classes.js.map
